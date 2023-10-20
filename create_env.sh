a4a6e2d5811a0c084313ac5cae84e9f54653299e
Gugus <gugus.deroubaix@gmail.com>
2023-10-20 15:30:02 +0200

    Create script for .env generation

---
+++ create_env.sh
@@ -0,0 +1,13 @@
+echo -n "Name for database : "
+read dbname
+echo -n "User for database : "
+read dbuser
+echo -n "Password for database : "
+read dbpass
+
+echo "PGDATA=db_data" > .env
+echo "POSTGRES_DB=$dbname" >> .env
+echo "POSTGRES_USER=$dbuser" >> .env
+echo "POSTGRES_PASSWORD=$dbpass" >> .env
+
+echo 'Generating done !'
