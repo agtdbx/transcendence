f12c022e650fd9046caa5ea38112083a4d0a36f6
Gugus <gugus.deroubaix@gmail.com>
2023-10-20 15:38:26 +0200

    update create_env

--- create_env.sh
+++ create_env.sh
@@ -10,6 +10,6 @@ echo "POSTGRES_DB=$dbname" >> .env
 echo "POSTGRES_USER=$dbuser" >> .env
 echo "POSTGRES_PASSWORD=$dbpass" >> .env
 
-npm install --global nx@latest
+npm install --global nx@latest 2>/dev/null || echo 'nx already installed'
 
 echo 'Generating done !'
