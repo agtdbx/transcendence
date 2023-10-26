aba5d78ef23b280a87d9cdf0a36fb2d079818dfb
Gugus <gugus.deroubaix@gmail.com>
2023-10-26 14:31:16 +0200

    Add nginx, modify backend env, modify volumes
    Nginx work good.
    You can access the site by localhost:4200. It automaticaly redirect you on the frontend.
    To access to the backend, the url must begin by /api.
    /adminer on the front of url redirect you in adminer.
    
    You can't access directly to frontend, backend or adminer. Pass through nginx.
    
    The backend folder is now close to frontend one, instead of inside it.
    The settings.py file now include os to get environement variable for db connection.
    No more wierd script to do this !
    
    Now, backend and frontend have their own volume. No more global volume for src.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -76,9 +78,9 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'transcendence_db',
-        'USER': 'Karl',
-        'PASSWORD': 'RockAndStone',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': 'postgresql',
         'PORT': '5432',
     }
