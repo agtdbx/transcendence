55d7fcfd32723e3652da171380f47da37c2754ac
Liam Flandrinck <lflandri@2E3.42angouleme.fr>
2023-11-29 13:29:38 +0100

    profil end (not db and css to connect with other branch)

--- backend/mystaticfiles/javascript/profil/achievement_and_stat.js
+++ backend/mystaticfiles/javascript/profil/achievement_and_stat.js
@@ -6,11 +6,11 @@
 /*   By: lflandri <lflandri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 15:24:40 by lflandri          #+#    #+#             */
-/*   Updated: 2023/11/24 17:28:56 by lflandri         ###   ########.fr       */
+/*   Updated: 2023/11/29 12:55:30 by lflandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//TODO connection to db + stat pannel
+//TODO connection to db
 
 function remove_pop()
 {
@@ -32,45 +32,34 @@ function casePup(content, before, title, img, description, grade)
 	const divText = document.createElement("div");
 	const titleText = document.createElement("h5");
 	const text = document.createElement("p");
-
 	image.style.width = "30%";
 	image.style.display = "inline-block";
 	image.src = img;
-
 	container.classList.add("col-sm-12");
 	container.classList.add("col-md-6");
 	container.classList.add("col-lg-6");
 	container.classList.add("col-xl-4");
-
 	box.style.display = "inline-block";
 	box.style.backgroundColor = "#000000";
 	box.style.border = "1px solid #FF9C00";
 	box.style.marginBottom = "15px";
 	box.style.textAlign = "left";
 	box.classList.add("mx-1");
-
 	divText.style.width = "70%";
 	divText.style.position = "relative";
 	divText.style.left = "30%";
 	divText.style.height = "30px";
 	divText.style.marginBottom = "-30px";
 	divText.style.fontSize = "10px";
-
-
 	titleText.style.color = "white";
 	titleText.textContent = title;
 	titleText.style.textAlign = "center";
 	titleText.style.marginTop = "1px";
 	titleText.style.marginBottom = "0px";
 	titleText.style.fontSize = "15px";
-
 	text.style.color = "white";
 	text.textContent = description;
 	text.style.marginTop = "5px";
-	
-
-
-
 	content.insertBefore(container, before);
 	container.insertBefore(box, before);
 	box.insertBefore(divText, null);
@@ -82,16 +71,17 @@ function casePup(content, before, title, img, description, grade)
 	{
 		const gradeContainer = document.createElement("div");
 		const gradeImage = document.createElement("img");
-
 		gradeImage.style.height = "30px";
 		gradeImage.style.marginBottom = "-30px";
 		gradeImage.style.marginTop = "0px";
 		gradeImage.style.position = "relative";
-		gradeImage.style.left = "40%";
-		gradeImage.style.top = "-15px";
+		gradeImage.style.left = "0%";
+		gradeImage.style.top = "-50px";
 		gradeImage.style.width = "25%"
-		
-		switch (grade) {
+		gradeContainer.style.height = "30px";
+		gradeContainer.style.marginBottom = "-30px";
+		switch (grade)
+		{
 			case 1:
 				gradeImage.src = "https://static.wikia.nocookie.net/deeprockgalactic_gamepedia_en/images/5/5e/Icons_complexity_1.png/revision/latest/scale-to-width-down/60?cb=20190210235110";
 				break;
@@ -103,11 +93,9 @@ function casePup(content, before, title, img, description, grade)
 				gradeImage.src = "https://static.wikia.nocookie.net/deeprockgalactic_gamepedia_en/images/1/1f/Icons_complexity_3.png/revision/latest/scale-to-width-down/60?cb=20190210235136";
 				break;
 		}
-		box.insertBefore(gradeContainer, null);
+		container.insertBefore(gradeContainer, null);
 		gradeContainer.insertBefore(gradeImage, null);
-
 	}
-
 }
 
 
@@ -118,53 +106,27 @@ function titlePup(content, before, title)
 	const titleImg = document.createElement("img");
 	const boscoRight = document.createElement("img");
 	const titlePop = document.createElement("div");
-	//const titleText = document.createElement("h4");
 	const separator = document.createElement("div");
-
 	titleImg.style.width = "100%";
-	//titleImg.style.marginTop = "-40%";
 	if (title === "Success")
 		titleImg.src = "/static/image/other/Icon_Forge_Mastery_achievement.webp";
-	
 	titlePop.style.display = "inline-block";
 	titlePop.style.width = "50%";
 	titlePop.style.marginLeft = "1%";
 	titlePop.style.marginRight = "1%";
-
-	// titleText.textContent = title;
-	// titleText.style.fontSize = "20px"
-	// titleText.style.position = "relative";
-	// titleText.style.left = "15%";
-	// titleText.style.top = "1vw";
-	// titleText.style.width = "50%";
-	// titleText.style.paddingTop = "40%";
-	// titleText.style.textAlign = "center";
-	// titleText.style.color = "white";
-	// titleText.style.height = "30px";
-	// titleText.style.marginBottom = "-30px";
-	// titleText.style.marginTop = "0px";
-
-	//boscoLeft.style.marginLeft = "39%";
 	boscoLeft.style.display = "inline-block";
 	boscoLeft.src = "https://cdn.discordapp.com/attachments/1166317817595969697/1166332408874946620/bosco_taunt.gif?ex=65535533&is=6540e033&hm=e7e769432fbbe26b8a7630b0e1a49b48d90b56b4df3b5721f4208cf9f3e8b0b4&";
 	boscoLeft.style.width = "24%";
 	boscoRight.style.display = "inline-block";
 	boscoRight.style.width = "24%";
 	boscoRight.src = "https://cdn.discordapp.com/attachments/1166317817595969697/1166332408874946620/bosco_taunt.gif?ex=65535533&is=6540e033&hm=e7e769432fbbe26b8a7630b0e1a49b48d90b56b4df3b5721f4208cf9f3e8b0b4&";
-
 	separator.style.marginBottom = "20px";
-		/* transformation css
-		-webkit-transform:rotate(180deg); //Safari 3.1+/Chrome
-		-o-transform:rotate(180deg);  //Opera 10.5+
-		-moz-transform: rotate(180deg); //Firefox 3.5+
-		*/
 	container.classList.add("col-md-8");
 	container.classList.add("col-lg-6");
 	container.classList.add("col-xl-4");
 	container.style.margin = "auto";
 	content.insertBefore(container, before);
 	container.insertBefore(boscoLeft, before);
-	//titlePop.insertBefore(titleText, null);
 	titlePop.insertBefore(titleImg, null);
 	container.insertBefore(titlePop, before);
 	container.insertBefore(boscoRight, before);
@@ -175,39 +137,31 @@ function titlePup(content, before, title)
 
 function createSuccess()
 {
-	//remove_pop();
 	console.log("create success panel");
 	const body = document.getElementById("body");
 	const containerContentSuccesBlock = document.createElement("div");
 	const contentSuccesBlock = document.createElement("div");
 	const succesBlock = document.createElement("div");
 	const gameDiv = document.getElementById("main_block_profil");
-
 	body.insertBefore(containerContentSuccesBlock, gameDiv);
 	containerContentSuccesBlock.insertBefore(contentSuccesBlock, null);
 	contentSuccesBlock.insertBefore(succesBlock, null);
-
 	containerContentSuccesBlock.id = "containerContentSuccesBlock";
 	containerContentSuccesBlock.style.position = "absolute";
 	containerContentSuccesBlock.style.width = window.innerWidth;
 	containerContentSuccesBlock.style.width = "100%";
-
 	contentSuccesBlock.classList.add("container");
 	contentSuccesBlock.id = "contentSuccessBlock";
 	contentSuccesBlock.style.margin = "auto";
 	succesBlock.id = "successBlock";
-	//succesBlock.style.position = "sticky";
-	//succesBlock.style.left = "";
 	succesBlock.style.margin = "auto";
-	succesBlock.style.backgroundColor = "rgba(40, 35, 23, 0.75)" ; /*"#282317"*/
+	succesBlock.style.backgroundColor = "rgba(40, 35, 23, 0.75)" ;
 	succesBlock.classList.add("row");
-	//succesBlock.classList.add("text-center");
 	succesBlock.style.borderCollapse = "separate";
 	succesBlock.style.textAlign = "center";
 	succesBlock.style.padding = "4%"
-	//succesBlock.style.height = "500px";
 	succesBlock.style.width = "70%";
-	succesBlock.style.zIndex = "1000";
+	containerContentSuccesBlock.style.zIndex = "1000";
 	titlePup(succesBlock, null, "Success");
 
 	for (let index = 0; index < 9; index++) {
@@ -219,7 +173,7 @@ function createSuccess()
 				null,
 				"ERR://23¤Y%/ ",
 				"https://static.wikia.nocookie.net/deeprockgalactic_gamepedia_en/images/1/14/Unknown_artifact_icon.png/revision/latest/scale-to-width-down/250?cb=20180519140040",
-				"You don't have the success",
+				"You don't have the achievement",
 				2);
 		}
 		else 
@@ -228,10 +182,88 @@ function createSuccess()
 				null,
 				"ERR://23¤Y%/ ",
 				"https://static.wikia.nocookie.net/deeprockgalactic_gamepedia_en/images/1/14/Unknown_artifact_icon.png/revision/latest/scale-to-width-down/250?cb=20180519140040",
-				"You don't have the success",
+				"You don't have the achievement",
 				-1);
 		}
 
+	}
+}
+
+function titlePup(content, before, title)
+{
+	const container = document.createElement("div");
+	const boscoLeft = document.createElement("img");
+	const titleImg = document.createElement("img");
+	const boscoRight = document.createElement("img");
+	const titlePop = document.createElement("div");
+	const separator = document.createElement("div");
+	titleImg.style.width = "100%";
+	if (title === "Success")
+		titleImg.src = "/static/image/other/Icon_Forge_Mastery_achievement.webp";
+	if (title === "Stats")
+		titleImg.src = "/static/image/other/Statistique_title.png";
+	titlePop.style.display = "inline-block";
+	titlePop.style.width = "50%";
+	titlePop.style.marginLeft = "1%";
+	titlePop.style.marginRight = "1%";
+	boscoLeft.style.display = "inline-block";
+	boscoLeft.src = "https://cdn.discordapp.com/attachments/1166317817595969697/1166332408874946620/bosco_taunt.gif?ex=65535533&is=6540e033&hm=e7e769432fbbe26b8a7630b0e1a49b48d90b56b4df3b5721f4208cf9f3e8b0b4&";
+	boscoLeft.style.width = "24%";
+	boscoRight.style.display = "inline-block";
+	boscoRight.style.width = "24%";
+	boscoRight.src = "https://cdn.discordapp.com/attachments/1166317817595969697/1166332408874946620/bosco_taunt.gif?ex=65535533&is=6540e033&hm=e7e769432fbbe26b8a7630b0e1a49b48d90b56b4df3b5721f4208cf9f3e8b0b4&";
+	separator.style.marginBottom = "20px";
+	container.classList.add("col-md-8");
+	container.classList.add("col-lg-6");
+	container.classList.add("col-xl-4");
+	container.style.margin = "auto";
+	container.style.zIndex = "1";
+	content.insertBefore(container, before);
+	container.insertBefore(boscoLeft, before);
+	titlePop.insertBefore(titleImg, null);
+	container.insertBefore(titlePop, before);
+	container.insertBefore(boscoRight, before);
+	content.insertBefore(separator, before);
+	
+}
+
+
+function createStats()
+{
+	console.log("create stats panel");
+	const body = document.getElementById("body");
+	const containerContentSuccesBlock = document.createElement("div");
+	const contentSuccesBlock = document.createElement("div");
+	const succesBlock = document.createElement("div");
+	const gameDiv = document.getElementById("main_block_profil");
+	body.insertBefore(containerContentSuccesBlock, gameDiv);
+	containerContentSuccesBlock.insertBefore(contentSuccesBlock, null);
+	contentSuccesBlock.insertBefore(succesBlock, null);
+	containerContentSuccesBlock.id = "containerContentSuccesBlock";
+	containerContentSuccesBlock.style.position = "absolute";
+	containerContentSuccesBlock.style.width = window.innerWidth;
+	containerContentSuccesBlock.style.width = "100%";
+	contentSuccesBlock.classList.add("container");
+	contentSuccesBlock.id = "contentSuccessBlock";
+	contentSuccesBlock.style.margin = "auto";
+	succesBlock.id = "successBlock";
+	succesBlock.style.margin = "auto";
+	succesBlock.style.backgroundColor = "rgba(40, 35, 23, 0.75)" ;
+	succesBlock.classList.add("row");
+	succesBlock.style.borderCollapse = "separate";
+	succesBlock.style.textAlign = "center";
+	succesBlock.style.padding = "4%"
+	succesBlock.style.width = "70%";
+	containerContentSuccesBlock.style.zIndex = "1000";
+	titlePup(succesBlock, null, "Stats");
+	for (let index = 0; index < 9; index++)
+	{
+			casePup(succesBlock,
+				null,
+				"Stats Name",
+				"https://static.wikia.nocookie.net/deeprockgalactic_gamepedia_en/images/1/14/Unknown_artifact_icon.png/revision/latest/scale-to-width-down/250?cb=20180519140040",
+				"Stats description",
+				-1);
 	}
 }
 
@@ -242,12 +274,13 @@ function createpup(type)
 	{
 		createSuccess()
 	}
+	if (type === "stats")
+	{
+		createStats()
+	}
 	setTimeout(function(){
 		const body = document.getElementById("body");
 		body.onclick = remove_pop;
 	}, 500);
-	
-	//console.log(body.onclick);
-
 }
   
--- backend/mystaticfiles/javascript/profil/contact.js
+++ backend/mystaticfiles/javascript/profil/contact.js
@@ -6,9 +6,245 @@
 /*   By: lflandri <lflandri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 15:27:08 by lflandri          #+#    #+#             */
-/*   Updated: 2023/11/24 17:01:42 by lflandri         ###   ########.fr       */
+/*   Updated: 2023/11/29 13:21:51 by lflandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //TODO all
 
+function removeContactPop()
+{
+	const popup = document.getElementById("containerContentSuccesBlock");
+	console.log("popup : ");
+	console.log(popup);
+	console.log("\n");
+	while (document.getElementById("containerContentSuccesBlock") != null)
+		document.getElementById("containerContentSuccesBlock").remove();
+	const body = document.getElementById("body");
+	body.onclick = null;
+}
+
+
+function contactCreate(content, before, name, img,status)
+{
+	const container = document.createElement("div");
+	const image = document.createElement("img");
+	const box = document.createElement("div");
+	const divText = document.createElement("div");
+	const titleText = document.createElement("h5");
+	container.style.padding = "2%";
+	container.style.paddingBottom = "0%";
+	image.style.width = "20%";
+	image.style.marginLeft = "0%";
+	image.style.maxWidth = "45px";
+	image.style.display = "inline-block";
+	image.src = img;
+	image.style.border = "3px solid #000000";
+	image.style.borderRadius = "600px";
+	if (status)
+		image.style.borderColor = "green";
+	else
+		image.style.borderColor = "red";
+	box.style.borderRadius = "600px";
+	box.style.display = "inline-block";
+	box.style.backgroundColor = "#000000";
+	box.style.border = "2px solid #FF9C00";
+	box.style.textAlign = "left";
+	box.style.width = "100%";
+	box.style.verticalAlign = "center";
+	box.style.maxHeight = "50px";
+	box.style.height = "11vw";
+	divText.style.verticalAlign = "center";
+	divText.style.width = "70%";
+	divText.style.position = "relative";
+	divText.style.left = "30%";
+	divText.style.height = "30px";
+	divText.style.marginBottom = "-30px";
+	divText.style.fontSize = "10px";
+	titleText.style.color = "white";
+	titleText.textContent = name;
+	titleText.style.textAlign = "left";
+	titleText.style.marginTop = "0%";
+	titleText.style.marginBottom = "2%";
+	titleText.style.fontSize = "30px";
+	content.insertBefore(container, before);
+	container.insertBefore(box, before);
+	box.insertBefore(divText, null);
+	box.insertBefore(image, null);
+	divText.insertBefore(titleText, null);
+}
+
+
+function contactPop(type, listContact)
+{
+	console.log("create contact panel");
+	const body = document.getElementById("body");
+	const containerContentSuccesBlock = document.createElement("div");
+	const contentSuccesBlock = document.createElement("div");
+	const succesBlock = document.createElement("div");
+	const gameDiv = document.getElementById("main_block_profil");
+	const title = document.createElement("h4");
+	body.insertBefore(containerContentSuccesBlock, gameDiv);
+	containerContentSuccesBlock.insertBefore(contentSuccesBlock, null);
+	contentSuccesBlock.insertBefore(succesBlock, null);
+	succesBlock.insertBefore(title, null);
+	containerContentSuccesBlock.id = "containerContentSuccesBlock";
+	containerContentSuccesBlock.style.position = "absolute";
+	containerContentSuccesBlock.style.width = window.innerWidth;
+	containerContentSuccesBlock.style.width = "100%";
+	contentSuccesBlock.id = "contentSuccessBlock";
+	contentSuccesBlock.style.margin = "auto";
+	succesBlock.id = "successBlock";
+	succesBlock.style.margin = "auto";
+	succesBlock.style.backgroundColor = "rgba(40, 35, 23, 0.75)" ;
+	succesBlock.style.borderCollapse = "separate";
+	succesBlock.style.textAlign = "center";
+	succesBlock.style.padding = "0%"
+	succesBlock.style.width = "50%";
+	succesBlock.style.maxWidth = "400px";
+	succesBlock.style.height = "500px";
+	succesBlock.style.overflowY = "scroll";
+	containerContentSuccesBlock.style.zIndex = "1000";
+	title.textContent = type;
+	title.style.backgroundColor = "black";
+	title.style.color = "white";
+	title.style.maxHeight = "50px";
+	title.style.padding = "2%";
+	title.style.verticalAlign = "center";
+	console.log("enter bl contact")
+	for (let index = 0; index < listContact.length; index++)
+	{
+		console.log("create : " + listContact[index]["name"])
+		contactCreate(succesBlock,
+				null,
+				listContact[index]["name"],
+				listContact[index]["pp"],
+				listContact[index]["status"]);
+	}
+}
+
+
+
+
+function ContactList(type)
+{
+	listContact =
+		[
+
+			{
+				"name" : "Enginer",
+				"pp" : "https://static.wikia.nocookie.net/deeprockgalactic_gamepedia_en/images/4/46/Engineer_portrait.png/revision/latest/scale-to-width-down/35?cb=20180519150041",
+				"status" : 0
+			},
+			{
+				"name" : "Gunner",
+				"pp" : "https://static.wikia.nocookie.net/deeprockgalactic_gamepedia_en/images/c/c2/Gunner_portrait.png/revision/latest/scale-to-width-down/35?cb=20180519150058",
+				"status" : 1
+			},
+			{
+				"name" : "Gunner",
+				"pp" : "https://static.wikia.nocookie.net/deeprockgalactic_gamepedia_en/images/c/c2/Gunner_portrait.png/revision/latest/scale-to-width-down/35?cb=20180519150058",
+				"status" : 1
+			},
+			{
+				"name" : "Gunner",
+				"pp" : "https://static.wikia.nocookie.net/deeprockgalactic_gamepedia_en/images/c/c2/Gunner_portrait.png/revision/latest/scale-to-width-down/35?cb=20180519150058",
+				"status" : 1
+			},
+			{
+				"name" : "Gunner",
+				"pp" : "https://static.wikia.nocookie.net/deeprockgalactic_gamepedia_en/images/c/c2/Gunner_portrait.png/revision/latest/scale-to-width-down/35?cb=20180519150058",
+				"status" : 1
+			},
+			{
+				"name" : "Gunner",
+				"pp" : "https://static.wikia.nocookie.net/deeprockgalactic_gamepedia_en/images/c/c2/Gunner_portrait.png/revision/latest/scale-to-width-down/35?cb=20180519150058",
+				"status" : 1
+			},
+			{
+				"name" : "Gunner",
+				"pp" : "https://static.wikia.nocookie.net/deeprockgalactic_gamepedia_en/images/c/c2/Gunner_portrait.png/revision/latest/scale-to-width-down/35?cb=20180519150058",
+				"status" : 1
+			},
+			{
+				"name" : "Gunner",
+				"pp" : "https://static.wikia.nocookie.net/deeprockgalactic_gamepedia_en/images/c/c2/Gunner_portrait.png/revision/latest/scale-to-width-down/35?cb=20180519150058",
+				"status" : 1
+			},
+			{
+				"name" : "Gunner",
+				"pp" : "https://static.wikia.nocookie.net/deeprockgalactic_gamepedia_en/images/c/c2/Gunner_portrait.png/revision/latest/scale-to-width-down/35?cb=20180519150058",
+				"status" : 1
+			},
+			{
+				"name" : "Gunner",
+				"pp" : "https://static.wikia.nocookie.net/deeprockgalactic_gamepedia_en/images/c/c2/Gunner_portrait.png/revision/latest/scale-to-width-down/35?cb=20180519150058",
+				"status" : 1
+			},
+			{
+				"name" : "Gunner",
+				"pp" : "https://static.wikia.nocookie.net/deeprockgalactic_gamepedia_en/images/c/c2/Gunner_portrait.png/revision/latest/scale-to-width-down/35?cb=20180519150058",
+				"status" : 1
+			},
+			{
+				"name" : "Gunner",
+				"pp" : "https://static.wikia.nocookie.net/deeprockgalactic_gamepedia_en/images/c/c2/Gunner_portrait.png/revision/latest/scale-to-width-down/35?cb=20180519150058",
+				"status" : 1
+			},
+			{
+				"name" : "Gunner",
+				"pp" : "https://static.wikia.nocookie.net/deeprockgalactic_gamepedia_en/images/c/c2/Gunner_portrait.png/revision/latest/scale-to-width-down/35?cb=20180519150058",
+				"status" : 1
+			},
+			{
+				"name" : "Gunner",
+				"pp" : "https://static.wikia.nocookie.net/deeprockgalactic_gamepedia_en/images/c/c2/Gunner_portrait.png/revision/latest/scale-to-width-down/35?cb=20180519150058",
+				"status" : 1
+			},
+			{
+				"name" : "Gunner",
+				"pp" : "https://static.wikia.nocookie.net/deeprockgalactic_gamepedia_en/images/c/c2/Gunner_portrait.png/revision/latest/scale-to-width-down/35?cb=20180519150058",
+				"status" : 1
+			},
+			{
+				"name" : "Gunner",
+				"pp" : "https://static.wikia.nocookie.net/deeprockgalactic_gamepedia_en/images/c/c2/Gunner_portrait.png/revision/latest/scale-to-width-down/35?cb=20180519150058",
+				"status" : 1
+			},
+			{
+				"name" : "Gunner",
+				"pp" : "https://static.wikia.nocookie.net/deeprockgalactic_gamepedia_en/images/c/c2/Gunner_portrait.png/revision/latest/scale-to-width-down/35?cb=20180519150058",
+				"status" : 1
+			},
+			{
+				"name" : "Gunner",
+				"pp" : "https://static.wikia.nocookie.net/deeprockgalactic_gamepedia_en/images/c/c2/Gunner_portrait.png/revision/latest/scale-to-width-down/35?cb=20180519150058",
+				"status" : 1
+			},
+			{
+				"name" : "Gunner",
+				"pp" : "https://static.wikia.nocookie.net/deeprockgalactic_gamepedia_en/images/c/c2/Gunner_portrait.png/revision/latest/scale-to-width-down/35?cb=20180519150058",
+				"status" : 1
+			},
+			{
+				"name" : "Gunner",
+				"pp" : "https://static.wikia.nocookie.net/deeprockgalactic_gamepedia_en/images/c/c2/Gunner_portrait.png/revision/latest/scale-to-width-down/35?cb=20180519150058",
+				"status" : 1
+			},
+			{
+				"name" : "Gunner",
+				"pp" : "https://static.wikia.nocookie.net/deeprockgalactic_gamepedia_en/images/c/c2/Gunner_portrait.png/revision/latest/scale-to-width-down/35?cb=20180519150058",
+				"status" : 1
+			},
+			{
+				"name" : "Gunner",
+				"pp" : "https://static.wikia.nocookie.net/deeprockgalactic_gamepedia_en/images/c/c2/Gunner_portrait.png/revision/latest/scale-to-width-down/35?cb=20180519150058",
+				"status" : 1
+			}
+		]
+	contactPop(type, listContact)
+	setTimeout(function(){
+		const body = document.getElementById("body");
+		body.onclick = removeContactPop;
+	}, 500);
+}
