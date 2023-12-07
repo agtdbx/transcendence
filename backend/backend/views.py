2e05bbb43a1c6b2a7c49a346294b2e213a57e3b0
Hugo De-min <hde-min@1E6.42angouleme.fr>
2023-12-07 16:22:03 +0100

    Merci Liam

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -6,7 +6,7 @@
 #    By: hde-min <hde-min@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/11/08 14:00:09 by lflandri          #+#    #+#              #
-#    Updated: 2023/12/04 14:32:10 by hde-min          ###   ########.fr        #
+#    Updated: 2023/12/07 16:20:09 by hde-min          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,29 +15,9 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import Http404, HttpResponse
 from django.shortcuts import render
 
-# def index(request):
-#     return render(request,"index.html")
+from db_test.models import User
+from db_test.models import Status
 
-def mainPage(request):
-  return render(request,"mainpage.html")
-
-def waitPage(request):
-  return render(request,"waitpage.html")
-
-def ladderPage(request):
-  return render(request,"ladder.html")
-
-def createtournaPage(request):
-  return render(request,"tournamentcreate.html")
-
-def tournaPage(request):
-  return render(request,"tournament.html")
-
-def cgp(request):
-  return render(request,"createGameRoom.html")
-
-def profil(request):
-    return render(request,"profil_content.html")
 
 # def testJS(request):
 #     return render(request,"other.html")
@@ -47,7 +27,7 @@ def testPY(request):
     return HttpResponse("success")
 
 
-def index2(request):
+def index(request):
     return render(request, 'index.html')
 
 randomstring = ["This is section 1.","This is section 2.", "This is section 3."]
@@ -56,7 +36,21 @@ def section(request, num):
     if num == 0:
         return render(request,"navbar.html")
     elif num == 1:
-        return render(request,"mainpage.html")
+        if request.method == 'POST':
+            username = request.POST.get('login')
+            idUser = request.POST.get('password')
+            idType = idUser
+
+            # Create a new patient entry in the database using the Patient model
+
+            status= Status(idStatus='0', name="Online")
+            status.save()
+
+            user = User(idUser=idUser, idType=idType, username=username, profilPicture="wrgsd", tokenJWT="wgdsv", money=1, idStatus=Status(idStatus='0', name="Online"))
+            user.save()
+            return render(request,"mainpage.html")
+        else:
+            return HttpResponse("Invalid request method.")
     elif num == 2:
         return render(request,"waitpage.html")
     elif num == 3:
@@ -69,3 +63,23 @@ def section(request, num):
         return render(request,"index.html")
     else:
         raise Http404('No such section')
+
+def user(request):
+    if request.method == 'POST':
+        username = request.POST.get('login')
+        idUser = request.POST.get('password')
+        idType = idUser
+
+        # Create a new patient entry in the database using the Patient model
+
+        status= Status(idStatus='0', name="Online")
+        status.save()
+
+        user = User(idUser=idUser, idType=idType, username=username, profilPicture="wrgsd", tokenJWT="wgdsv", money=1, idStatus=Status(idStatus='0', name="Online"))
+        user.save()
+
+
+
+        return render(request,"mainpage.html")
+    else:
+        return HttpResponse("Invalid request method.")
