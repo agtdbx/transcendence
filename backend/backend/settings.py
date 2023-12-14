8ee989fa0b7e5d780262ea755d077dbda7ab1d24
Gugus <gugus.deroubaix@gmail.com>
2023-12-14 12:10:19 +0100

    update create_env.sh to secure the django key and 42 api key

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v21tl+u&*htdopsf2jl6w0)_&ao^#^*zwagrmp&sb1+sg*uq'
+# SECRET_KEY = 'django-insecure-v21tl+u&*htdopsf2jl6w0)_&ao^#^*zwagrmp&sb1+sg*uq'
+SECRET_KEY = os.getenv('DJANGO_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- create_env.sh
+++ create_env.sh
@@ -1,13 +1,23 @@
+echo "PGDATA='db_data'" > .env
+
 echo -n "Name for database : "
 read dbname
+echo "POSTGRES_DB='$dbname'" >> .env
+
 echo -n "User for database : "
 read dbuser
+echo "POSTGRES_USER='$dbuser'" >> .env
+
 echo -n "Password for database : "
 read dbpass
+echo "POSTGRES_PASSWORD='$dbpass'" >> .env
 
-echo "PGDATA=db_data" > .env
-echo "POSTGRES_DB=$dbname" >> .env
-echo "POSTGRES_USER=$dbuser" >> .env
-echo "POSTGRES_PASSWORD=$dbpass" >> .env
+echo -n "Django secret key : "
+read djangokey
+echo "DJANGO_KEY='$djangokey'" >> .env
 
-echo 'Generating .nev done !'
+echo -n "42 api key : "
+read apikey
+echo "API_KEY='$apikey'" >> .env
+
+echo 'Generating .env done !'
