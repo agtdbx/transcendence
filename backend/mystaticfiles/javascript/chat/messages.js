ffffb6fa09b48a8357b9ede9fe75355464858c2f
Gugus <gugus.deroubaix@gmail.com>
2024-01-16 15:44:28 +0100

    message are now visible

--- backend/mystaticfiles/javascript/chat/messages.js
+++ backend/mystaticfiles/javascript/chat/messages.js
@@ -50,14 +50,17 @@ function enableChatConnection()
 		console.log("Data parse :", data);
 		const channel = data['channel'];
 		const message = data['message'];
-		console.log("Message recieved :", message);
-		console.log("Channel target :", channelTarget);
+		const username = data['username'];
+		// const pp = data['pp'];
+		const pp = "https://static.wikia.nocookie.net/deeprockgalactic_gamepedia_en/images/c/c2/Gunner_portrait.png/revision/latest/scale-to-width-down/35?cb=20180519150058";
+		let date = data['date'];
+		date = date.substring(11, 19);
+		console.log("DATE :", date);
 		if (channel == channelTarget)
 		{
 			console.log("displayMessage");
+			addNewMessage(message, username, pp, date);
 		}
-		else
-			console.log("Not message display");
 	};
 
 	chatSocket.onclose = function(e) {
@@ -99,4 +102,82 @@ function sendMessage(input)
 	console.log("Try to send the message");
 	sendMessageToServer(input.value, "general");
 	input.value = "";
+}
+
+
+function createMessage(message, username, pp, date)
+{
+	let messageDiv = document.createElement('tr');
+	let nameCase = document.createElement('td');
+	let nameTXT = document.createElement('h5');
+	let dateTXT = document.createElement('p');
+	let imgCase = document.createElement('td');
+	let img = document.createElement('img');
+	let messageCase = document.createElement('td');
+	let messageTXT = document.createElement('p');
+
+	nameCase.style.width = "10%";
+	nameCase.style.textAlign = "center";
+	nameCase.style.verticalAlign = "top";
+	nameTXT.style.marginTop = "5px";
+	nameTXT.style.marginBottom = "2px";
+	nameTXT.style.color = "white";
+	nameTXT.textContent = username;
+	nameTXT.style.fontSize = "0.9vw";
+	dateTXT.style.marginTop = "0px";
+	dateTXT.style.marginBottom = "0px";
+	dateTXT.style.color = "white";
+	dateTXT.textContent = date;
+	dateTXT.style.fontSize = "0.7vw";
+	imgCase.style.verticalAlign = "top";
+	imgCase.style.width = "10%";
+	img.style.width = "100%";
+	img.src = pp
+	messageCase.style.width = "85%";
+	messageCase.style.paddingLeft = "5%";
+	messageCase.style.verticalAlign = "top";
+	messageTXT.style.marginTop = "5px";
+	messageTXT.style.color = "white";
+	messageTXT.style.fontSize = "1vw";
+	messageTXT.textContent = message;
+
+	messageDiv.insertBefore(nameCase, null);
+	messageDiv.insertBefore(imgCase, null);
+	messageDiv.insertBefore(messageCase, null);
+	nameCase.insertBefore(nameTXT, null);
+	nameCase.insertBefore(dateTXT, null);
+	imgCase.insertBefore(img, null);
+	messageCase.insertBefore(messageTXT, null);
+
+	return messageDiv;
+}
+
+
+function addOldMessage(message, username, pp, date)
+{
+	let chat = document.getElementById("main-page-chat-write");
+
+	if (chat == null)
+		chat = document.getElementById("createGameChat");
+	if (chat == null)
+		chat = document.getElementById("tournamentrect1");
+	if (chat == null)
+		return ;
+
+	chat.after(createMessage(message, username, pp, date));
+}
+
+
+function addNewMessage(message, username, pp, date)
+{
+	let chat = document.getElementById("main-page-chat-write");
+
+	if (chat == null)
+		chat = document.getElementById("createGameChat");
+	if (chat == null)
+		chat = document.getElementById("tournamentrect1");
+	if (chat == null)
+		return ;
+
+	chat.insertBefore(createMessage(message, username, pp, date), null);
 }
