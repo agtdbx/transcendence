bd596de3e5dc532ce57e1e05c7c2e9587a7c568c
Liam Flandrinck <lflandri@2A4.42angouleme.fr>
2024-01-30 19:00:22 +0100

    popup friend upgrade

--- backend/mystaticfiles/javascript/user_relation/friend_request.js
+++ backend/mystaticfiles/javascript/user_relation/friend_request.js
@@ -6,10 +6,135 @@
 /*   By: lflandri <lflandri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 22:32:37 by lflandri          #+#    #+#             */
-/*   Updated: 2024/01/16 23:17:17 by lflandri         ###   ########.fr       */
+/*   Updated: 2024/01/30 18:58:25 by lflandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
+var popupCreatedCount = 0;
+
+function comfirmFriendRequest(user)
+{
+	let body = document.getElementById('body');
+	document.getElementById("Page").classList.add("blur");
+	let container = document.createElement('div');
+	let popup = document.createElement('div');
+	let text = document.createElement('p');
+	let acceptBtn = document.createElement('button');
+	let refusedBtn = document.createElement('button');
+
+	container.style.position = "absolute";
+	container.style.width = "100%";
+	container.style.height = "1080px";
+	container.style.top = "0px";
+	popup.style.position = "relative";
+	popup.style.boxShadow = "26px 21px 27px 5px #000000";
+	popup.style.left = "35%";
+	popup.style.top = "400px";
+	popup.style.height = "200px";
+	popup.style.backgroundColor = "rgba(18,16,11, 0.75)";
+	popup.style.padding = "2%";
+	popup.style.width = "30%";
+	popup.style.borderRadius = "40px 0px 40px 0px";
+	text.style.color = "white";
+	text.textContent = "You have a friend request of " + user + ".\n\nDo you accept it ?";
+	acceptBtn.classList.add("btn-drg")
+	refusedBtn.classList.add("btn-drg")
+	acceptBtn.textContent = "Accept"
+	refusedBtn.textContent = "Refuse"
+	acceptBtn.style.width = "20%"
+	acceptBtn.style.margin = "15%"
+	refusedBtn.style.width = "20%"
+	refusedBtn.style.margin = "15%"
+	acceptBtn.onclick = function ()
+	{
+		document.getElementById("Page").classList.remove("blur");
+		popupCreatedCount--;
+		container.remove()
+		document.getElementById("data-request-relation-input").value = user;
+		const form = document.getElementById('data-request-relation');
+		const data = new FormData(form);
+		fetch("https://" + window.location.hostname + ":4200/acceptfriends",
+		{
+			method: 'POST',
+			body: data,
+			cache: "default"
+		})
+				.then(response => response.json())
+				.then (jsonData => {
+					if (! jsonData["success"])
+					{
+						console.error(jsonData["content"])
+						return ;
+					}
+					console.log("received from friendresponse : ")
+					console.log(jsonData)
+					
+				})
+				.catch(error => {
+					console.log("erreur from getlistefriendrequest : ")
+					console.error(error)
+				})
+	};
+	refusedBtn.onclick = function ()
+	{
+		document.getElementById("Page").classList.remove("blur");
+		popupCreatedCount--;
+		container.remove()
+		document.getElementById("data-request-relation-input").value = user;
+		const form = document.getElementById('data-request-relation');
+		const data = new FormData(form);
+		fetch("https://" + window.location.hostname + ":4200/refusefriends",
+			{
+				method: 'POST',
+				body: data,
+				cache: "default"
+			})
+					.then(response => response.json())
+					.then (jsonData => {
+						if (! jsonData["success"])
+						{
+							console.error(jsonData["content"])
+							return ;
+						}
+						console.log("received from friendresponse : ")
+						console.log(jsonData)
+						
+					})
+					.catch(error => {
+						console.log("erreur from getlistefriendrequest : ")
+						console.error(error)
+					})
+	};
+	body.appendChild(container);
+	container.appendChild(popup);
+	popup.appendChild(text);
+	popup.appendChild(acceptBtn);
+	popup.appendChild(refusedBtn);
+}
+
+async function sendRequestToClient(jsonData)
+{
+	for (const user of jsonData["listRequest"])
+	{
+		document.getElementById("data-request-relation-input").value = user;
+		const form = document.getElementById('data-request-relation');
+		const data = new FormData(form);
+		console.log("request to : " + user)
+		let popupInterval = setInterval(
+			function ()
+			{	if (popupCreatedCount === 0)
+				{
+					popupCreatedCount++;
+					comfirmFriendRequest(user);
+					clearInterval(popupInterval);
+				}
+			},
+			100);
+
+	}
+	
+}
 
 const form = document.getElementById('data-request-relation');
 const data = new FormData(form);
@@ -29,41 +154,7 @@ fetch("https://" + window.location.hostname + ":4200/getlistefriendrequest",
 				console.error(jsonData["content"])
 				return ;
 			}
-			for (const user of jsonData["listRequest"])
-			{
-				document.getElementById("data-request-relation-input").value = user;
-				const form = document.getElementById('data-request-relation');
-				const data = new FormData(form);
-				let url = ""
-				if (confirm("You have a friend request of " + user + ".\n\nDo you accept it ?"))
-					url = "https://" + window.location.hostname + ":4200/acceptfriends"
-				else
-					url = "https://" + window.location.hostname + ":4200/refusefriends"
-				fetch(url,
-				{
-					method: 'POST',
-					body: data,
-					cache: "default"
-				})
-						.then(response => response.json())
-						.then (jsonData => {
-							if (! jsonData["success"])
-							{
-								console.error(jsonData["content"])
-								return ;
-							}
-							else
-							{
-								console.log("received from friendresponse : ")
-								console.log(jsonData)
-							}
-							
-						})
-						.catch(error => {
-							console.log("erreur from getlistefriendrequest : ")
-							console.error(error)
-						})
-			}
+			sendRequestToClient(jsonData);
 			
 		})
 		.catch(error => {
